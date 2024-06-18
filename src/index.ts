9e79054509fcb5d77043571e0746e09e361a0800
ymukha@piesoft.us <ymukha@piesoft.us>
2024-06-18 11:41:02 +0300

    rxjs

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,3 @@ forkJoin({
     dataGitLab: ajax.getJSON('https://gitlab.com/api/v4/projects?search=nodejs')
 })
     .subscribe(console.log)
-//
-// const data$ = ajax.getJSON('https://api.github.com/search/repositories?q=rxjs');
-//
-// data$.subscribe((value) => console.log('data$ value', value));
-//
-// const dataGitLab$ = ajax.getJSON('https://gitlab.com/api/v4/projects?search=nodejs');
-//
-// dataGitLab$.subscribe((value) => console.log('dataGitLab$ value', value));
